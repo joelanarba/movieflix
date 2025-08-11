739ef1bf30ac901f460f7beed49bcbc0ea1254b5
Joel Anarba <157653692+joelanarba@users.noreply.github.com>
2025-08-11 11:29:24 +0000

    Fix: Fix dynamic routes for Vercel deployment

--- next.config.js
+++ next.config.js
@@ -4,9 +4,6 @@ const nextConfig = {
     ignoreDuringBuilds: true,
   },
   images: { unoptimized: true },
-  output: 'export',
-  trailingSlash: true,
-  distDir: 'out',
 };
 
 module.exports = nextConfig;
