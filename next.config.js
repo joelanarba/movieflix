835aceb757f0bfceaadeb7a281ed4f2ebaac7451
Joel Anarba <157653692+joelanarba@users.noreply.github.com>
2025-08-11 11:17:43 +0000

    config: Configure Next.js for Vercel static deployment

--- next.config.js
+++ next.config.js
@@ -4,6 +4,9 @@ const nextConfig = {
     ignoreDuringBuilds: true,
   },
   images: { unoptimized: true },
+  output: 'export',
+  trailingSlash: true,
+  distDir: 'out',
 };
 
 module.exports = nextConfig;
